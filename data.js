c77dcf52ebbf790e94d7a307904fef3a769d3f87
Rich Abdill <richard.abdill@gmail.com>
2018-09-03 16:47:24 -0500

    Adding more events

--- data.js
+++ data.js
@@ -2,10 +2,127 @@ entries = [
   {
     "title": "BLINK: Life Sciences Seminars at the U of M",
     "dept": "multiple",
-    "topics": "&ldquo;more than 40 seminar series that share in common a focus on scientific research at the University of Minnesota.&rdquo;",
+    "topics": "more than 40 seminar series that share in common a focus on scientific research at the University of Minnesota.",
     "frequency": "multiple",
     "time": "multiple",
     "location": "multiple",
     "url": "https://cbs.umn.edu/research/blink"
+  },
+  {
+    "title": "Seminar Series",
+    "dept": "Microbiology, Immunology and Cancer Biology (MICaB)",
+    "topics": "",
+    "frequency": "weekly on Monday",
+    "time": "noon",
+    "location": "1-125 CCRB",
+    "url": "http://www.micab.umn.edu/news/invitedsemseries.html"
+  },
+  {
+    "title": "Division of Biostatistics seminar series",
+    "dept": "School of Public Health (SPH)",
+    "topics": "research-focused talks by visiting faculty from a wide range of institutions, and faculty from the University of Minnesota",
+    "frequency": "weekly on Monday",
+    "time": "noon",
+    "location": "mostly Mayo 3-125",
+    "url": "http://www.sph.umn.edu/academics/divisions/biostatistics/bioevents/seminars/"
+  },
+  {
+    "title": "Data Science Seminars",
+    "dept": "Institute for Mathematics and its Applications",
+    "topics": "",
+    "frequency": "weekly on Monday",
+    "time": "1:25&ndash;2:25 p.m.",
+    "location": "Lind Hall 305",
+    "url": "https://www.ima.umn.edu/seminar/dsseminar"
+  },
+  {
+    "title": "Cancer Center Seminar Series",
+    "dept": "Masonic Cancer Center",
+    "topics": "",
+    "frequency": "weekly on Tuesday",
+    "time": "9&ndash;10 a.m.",
+    "location": "1-125 CCRB",
+    "url": "https://www.cancer.umn.edu/education-training/cancer-center-seminar-series"
+  },
+  {
+    "title": "Seminars",
+    "dept": "Center for Genome Engineering",
+    "topics": "",
+    "frequency": "select Thursdays",
+    "time": "4 p.m.",
+    "location": "1-125 CCRB",
+    "url": "http://www.cge.umn.edu/seminars"
+  },
+  {
+    "title": "Friday Lab Meetings",
+    "dept": "Developmental Biology Center",
+    "topics": "",
+    "frequency": "weekly on Friday",
+    "time": "unknown",
+    "location": "unknown",
+    "url": "https://cbs.umn.edu/academics/departments/gcd/dbc/seminars-events"
+  },
+  {
+    "title": "Seminar Series",
+    "dept": "Neuroscience",
+    "topics": "",
+    "frequency": "weekly on Friday",
+    "time": "11:45 a.m.",
+    "location": "2-101 NHH",
+    "url": "https://neurosci.umn.edu/education/seminars"
+  },
+  {
+    "title": "Seminars and Awards",
+    "dept": "Medicinal Chemistry",
+    "topics": "",
+    "frequency": "weekly on Tuesday",
+    "time": "3:30&ndash;5 p.m.",
+    "location": "1-451 Moos Tower",
+    "url": "https://www.pharmacy.umn.edu/departments/medicinal-chemistry/seminars-and-awards"
+  },
+  {
+    "title": "Seminars",
+    "dept": "Precision Medicine and Health Consortium",
+    "topics": "",
+    "frequency": "occasionally",
+    "time": "",
+    "location": "",
+    "url": "http://pmhc.umn.edu/upcoming-seminar-symposiums"
+  },
+  {
+    "title": "Colloquia",
+    "dept": "Computer Science &amp; Engineering",
+    "topics": "",
+    "frequency": "weekly on Monday",
+    "time": "11:15&ndash;12:15",
+    "location": "3-180 Keller Hall",
+    "url": "https://www.cs.umn.edu/activity/talks/colloquia"
+  },
+  {
+    "title": "Lecture Series",
+    "dept": "Lillehei Heart Institute",
+    "topics": "",
+    "frequency": "weekly on Wednesday",
+    "time": "12&ndash;1 p.m.",
+    "location": "1-125 CCRB",
+    "url": "https://www.heart.umn.edu/news-events/lillehei-heart-institute-lecture-series"
+  },
+  {
+    "title": "Weekly Research Conference",
+    "dept": "Stem Cell Institute",
+    "topics": "",
+    "frequency": "weekly on Wednesday",
+    "time": "4 p.m.",
+    "location": "1-110 MTRF",
+    "url": "https://www.heart.umn.edu/news-events/lillehei-heart-institute-lecture-series"
+  },
+  {
+    "title": "Colloquium Series",
+    "dept": "Center for Neurobehavioral Development",
+    "topics": "",
+    "frequency": "monthly on Tuesday",
+    "time": "4 p.m.",
+    "location": "717 Delaware Building, Room 330",
+    "url": "https://cnbd.umn.edu/cnbd-events"
   },
 ]
