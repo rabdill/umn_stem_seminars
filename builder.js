6f7d3d2854fa491426c8bbd97fcdc8ba9e303144
Rich Abdill <richard.abdill@gmail.com>
2018-09-03 16:47:17 -0500

    Formatting

--- builder.js
+++ builder.js
@@ -1,12 +1,14 @@
 table = document.getElementById("seminars");
-cols = ["title", "dept", "topics", "frequency", "time", "location", "url"];
+cols = ["title", "dept", "topics", "frequency", "time", "location"];
 
 for(var i=0, entry; entry=entries[i]; i++) {
   row = table.insertRow(-1);
   for(var j=0, col; col=cols[j]; j++) {
     cell = row.insertCell(j);
-    if(col == "url") {
-      contents = "<a target='_blank' class='link-flip' href='" + entry[col] + "'>" + entry[col] + "</a>";
+    if(col == "title") {
+      contents = "<a target='_blank' class='link-flip' href='" + entry["url"] + "'>" + entry["title"] + "</a>";
+    } else if(col == "topics" && entry["topics"] && entry["topics"] != "") {
+      contents = "&ldquo;" + entry["topics"] + "&rdquo;";
     } else {
       contents = entry[col];
     }
